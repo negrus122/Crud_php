6d2404bd50e923b016b7e9ede12bbc5daf24f2bd
Ricardo Nahuel Sosa <109368359+negrus122@users.noreply.github.com>
2024-04-08 18:14:53 -0300

    Add files via upload

---
+++ Crud/Config/app.php
@@ -0,0 +1,8 @@
+<?php 
+
+    const APP_URL="http://localhost/Crud/";
+    const APP_NAME="CRUD POO MySQL";
+    const APP_SESSION_NAME="CRUD";
+
+
+    date_default_timezone_set("America/Argentina/Cordoba");
